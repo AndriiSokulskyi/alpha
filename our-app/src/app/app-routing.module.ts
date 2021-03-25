0687304b802b0b372ffd3a61e7ee92b1deb926f3
AndriiSokulskyi <andrii.sokulskyi@lnu.edu.ua>
2021-03-25 14:08:05 +0200

    016 registration page routing done

--- our-app/src/app/app-routing.module.ts
+++ our-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    loadChildren: () => import('./components/registration-page/registration-page.module').then(m => m.RegistrationPageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ our-app/src/app/components/registration-page/registration-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+
+
+
+@NgModule({
+  declarations: [RegistrationPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegistrationPageModule { }
