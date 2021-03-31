5ef4e259e96224f41ce15046adbb42de87a36f26
Yura Fostiak <jura.fostyak@gmail.com>
2021-04-01 01:22:33 +0300

    Added Login Page (without validation)
    Added Login Page, test version of header with ROUTES. Without validation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+
+const routes = [
+  {path: '', component: LoginComponent},
+  {path: 'home-test', component: HomePageComponent},
+  {path: 'signup', component: RegistrationComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
