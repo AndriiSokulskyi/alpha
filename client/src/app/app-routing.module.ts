565dd5a4f0e1dcd4052a01855352cf1ff6e38919
Yura Fostiak <jura.fostyak@gmail.com>
2021-04-10 00:36:09 +0300

    Cleared dev
    Added header component
    Cleared app.component.ts, .html
    Added links for font-awesome and imported fonts with basic styles in style.scss

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', component: HeaderComponent},
+  // {path: 'test', component: TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
